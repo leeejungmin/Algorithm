e3052fa2af53943297b7a13cc5c1702cad7cdaa3
jungminlee <dlwjdals6424@naver.com>
2022-02-13 15:18:45 +0900

    Hot_2

--- Heap/Hot.py
+++ Heap/Hot.py
@@ -1,3 +1,25 @@
+import heapq
 def solution(scoville, K):
     answer = 0
+    next_ = 0
+    heapq.heapify(scoville)
+    #c = a + b*2
+    #heapq.pop(0),heapq.pop(1)
+    
+    while True:
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        next_=a + b*2
+        print(a,b,next_)
+        answer+=1
+        if next_ > K:
+          break
+        else:
+            heapq.heappush(scoville,next_)
+    
+
     return answer
+
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+print(solution(scoville, K))
